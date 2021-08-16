cfa4a715a7f5713d1ee48379b5508c4e9072d232
Henrik Heymann <henrik.heymann@ipt.fraunhofer.de>
2021-08-16 09:25:18 +0200

    production and training data folders renamed

--- configuration.py
+++ configuration.py
@@ -1,10 +1,10 @@
-TRAINING_DATA_FILE =    "./data/training/uci-secom.csv"
-VALIDATION_DATA_FILE =  "./data/production/2008-12-10.csv"
-VALIDATION_DATA_FILE_2 =  "./data/production/2008-10-15.csv"
-HOLDOUT_DATA_FILE =     "./data/training/uci-secom.csv"
+TRAINING_DATA_FILE =    "./data/training_data/uci-secom.csv"
+VALIDATION_DATA_FILE =  "./data/production_data/2008-12-10.csv"
+VALIDATION_DATA_FILE_2 =  "./data/production_data/2008-10-15.csv"
+HOLDOUT_DATA_FILE =     "./data/training_data/uci-secom.csv"
 
 OBJECT_FOLDER =         "./model_pipeline/objects"
 
-PRODUCTION_DATA_FOLDER ="./data/production/"
+PRODUCTION_DATA_FOLDER ="./data/production_data/"
 
 VERSION_NUMBER = "1.0.0"
